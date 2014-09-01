942a09bb3d0138ba665e5bcb323b9b1a4584be11
lowoi <lowoi@msn.com>
2014-09-01 17:03:25 +0000

    Flash MAC througth web

---
+++ wrtnode/FlashMAC/README.mk
@@ -0,0 +1,17 @@
+Introduction
+===
+
+If you want to write MAC, you must ensure that mtd factory is writable.
+Remove the factory's read-only properties in the WRTNODE.dts.
+	
+vim WRTNODE.dts
+......
+	factory: partition@40000 {
+		label = "factory";
+		reg = <0x40000 0x10000>;
+		#read-only;
+	};
+......
+
+
+Download WRTnodemacjsonp.html and How to flash the MAC via the web.doc
