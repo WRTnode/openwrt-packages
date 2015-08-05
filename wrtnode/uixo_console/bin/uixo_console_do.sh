b4ab2f9a073d45e54095ce62280bfcc6cfae9157
schumy <schumy.wrtnode.com>
2015-08-05 23:07:35 +0800

    change uixo_console to uixo_console package. modified Makefiles

--- wrtnode/uixo_console/bin/uixo_console_do.sh
+++ wrtnode/uixo_console/bin/uixo_console_do.sh
@@ -4,14 +4,15 @@ MESSAGE=
 
 case $1 in
     "-m")
-        MESSAGE="[1234:1:0:0:0:$3:$2:mkport]\n"
+        MESSAGE="[1234:1:0:0:0:$3:$2:mkport] 0"
+        /bin/client_console 127.0.0.1 ${MESSAGE}
         ;;
     "-r")
-        MESSAGE="[1234:1:0:0:0:0:$2:rmport]\n"
+        MESSAGE="[1234:1:0:0:0:0:$2:rmport] 0"
         ;;
     "-s")
         DATA=$3
-        MESSAGE="[1234:${#DATA}:0:${DATA}:$4:0:$2:hlport]\n"
+        MESSAGE="[1234:${#DATA}:0:${DATA}:$4:0:$2:hlport] 0"
         ;;
     *)
         echo "Illegal parameters!"
@@ -20,7 +21,8 @@ case $1 in
 esac
 
 #echo {\"objname\":\"uixo_console\",\"data\":\"${MESSAGE}\",\"len\":${#MESSAGE}}
-ubus call uixo_manager wagent {\"objname\":\"uixo_console\",\"data\":\"${MESSAGE}\",\"len\":${#MESSAGE}}
+#ubus call uixo_manager wagent {\"objname\":\"uixo_console\",\"data\":\"${MESSAGE}\",\"len\":${#MESSAGE}}
 #ubus call uixo_manager ragent {\"objname\":\"uixo_console\",\"len\":2}
+
 
 echo 0
